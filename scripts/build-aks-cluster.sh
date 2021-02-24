5335864d8010fd3038fd76258f514516f8c66a2a
dekt <dekel@vmware.com>
2021-02-23 20:06:45 -0800

    move init and pipeline scripts to /scripts

---
+++ scripts/build-aks-cluster.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+#################### functions #######################
+
+#create cluster
+#	params: cluster-name, number-of-nodes
+create-cluster() {
+	
+	#rm ~/.kube/config
+
+	echo
+	echo "==========> Creating AKS cluster named $1 with $2 nodes ..."
+	echo
+	
+	az login -u $AZURE_USER -p $AZURE_PASSWORD
+	
+	az group create --name $RESOURCE_GROUP --location westus
+
+	az aks create --resource-group $RESOURCE_GROUP --name $1 --node-count $2 --generate-ssh-keys
+
+	az aks get-credentials --resource-group $RESOURCE_GROUP --name $1
+	
+	./install-ingress-controller.sh
+	
+}
+
+#delete cluster
+#	params: cluster name
+delete-cluster() {
+	
+	az login -u $AZURE_USER -p $AZURE_PASSWORD
+
+	echo
+	echo "==========> Starting deletion of AKS cluster: $1"
+	echo
+
+	az group delete -n $RESOURCE_GROUP --yes --no-wait
+
+	az aks delete --name $1 --resource-group $RESOURCE_GROUP --yes --no-wait
+
+}
+
+#incorrect usage
+incorrect-usage() {
+	
+	echo "Incorrect usage. Required: create/delete , cluster-name  , number-of-worker-nodes"
+	exit
+}
+
+
+#################### main #######################
+
+source ../secrets/config-values.txt
+
+case $1 in 
+create)
+	create-cluster $2 $3
+	;;
+delete)
+	delete-cluster $2
+  	;;
+*)
+  	incorrect-usage
+  	;;
+esac
+
+
