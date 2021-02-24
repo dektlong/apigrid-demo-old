d1886a21c52b37331d030e2bbe40a8a60beba647
dekt <dekel@vmware.com>
2021-02-23 23:42:36 -0800

    local-development-completed

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -26,7 +26,7 @@ deploy-backend() {
     echo
     echo "=========> Apply backend app, service and routes ..."
     echo    
-    #kubectl set image deployment dekt4pets-backend-app my-nginx=nginx:1.12.0
+    #kubectl set image deployment dekt4pets-backend-app $IMG_REGISTRY_URL/$IMG_REGISTRY_APP_REPO/$BACKEND_TBS_IMAGE:$APP_VERSION 
     kustomize build backend | kubectl apply -f -
     
 }
