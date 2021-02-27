5d071d18c1b897c3e4d72c1a96454d08f99af63f
dekt <dekel@vmware.com>
2021-02-26 20:13:40 -0800

    local-development-completed

--- hub/run-local-api-hub-server.sh
+++ hub/run-local-api-hub-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export APIHUB_SOURCE_URLS="http://scg-openapi.apps.dekt.io/openapi"
+export APIHUB_SOURCE_URLS="http://scg-openapi.new.dekt.io/openapi"
 
 java -jar ~/Dropbox/Work/code/api-hub/api-hub-server/build/libs/api-hub-server-0.0.1-SNAPSHOT.jar
