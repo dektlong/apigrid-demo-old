c05a4cb003b4a1e0306c65871f00fb6035c85b93
dektlong <dekel.tankel@gmail.com>
2021-02-26 20:28:17 -0800

    Update run-local-api-hub-server.sh

--- hub/run-local-api-hub-server.sh
+++ hub/run-local-api-hub-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export APIHUB_SOURCE_URLS="http://scg-openapi.new.dekt.io/openapi"
+export APIHUB_SOURCE_URLS="http://scg-openapi.{REPLACED_ME_BEFORE_RUNNING_SCRIPT}/openapi"
 
-java -jar ~/Dropbox/Work/code/api-hub/api-hub-server/build/libs/api-hub-server-0.0.1-SNAPSHOT.jar
+java -jar {REPLACE_ME_BEFORE_RUNNING_SCRIPT}
