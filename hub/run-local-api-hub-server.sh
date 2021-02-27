72a16fd0d53419939011c24ccc581a303c637668
dektlong <dekel.tankel@gmail.com>
2021-02-26 20:37:05 -0800

    Update run-local-api-hub-server.sh

--- hub/run-local-api-hub-server.sh
+++ hub/run-local-api-hub-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export APIHUB_SOURCE_URLS="http://scg-openapi.{REPLACED_ME_BEFORE_RUNNING_SCRIPT}/openapi"
+export APIHUB_SOURCE_URLS="http://scg-openapi.{REPLACED_IN_RUNTIME}/openapi"
 
-java -jar {REPLACE_ME_BEFORE_RUNNING_SCRIPT}
+java -jar api-hub-server-0.0.1-SNAPSHOT.jar #for now you will need to obtain the jar locally and modify this script
