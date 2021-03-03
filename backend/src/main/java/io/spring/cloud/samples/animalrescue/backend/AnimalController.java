cd044e94dc53c975f37e62a399ac3338f9cc9c2d
dekt <dekel@vmware.com>
2021-03-03 11:33:44 -0800

    add adopter-check api

--- backend/src/main/java/io/spring/cloud/samples/animalrescue/backend/AnimalController.java
+++ backend/src/main/java/io/spring/cloud/samples/animalrescue/backend/AnimalController.java
@@ -36,10 +36,10 @@ public class AnimalController {
     	String adopterId = adopter.getName();
     
 		//verify adoption history - use the Hub verified adoption-history API call
-		LOGGER.info("adopterId " + adopterId " has succefully completed adoption history check");
+		LOGGER.info("adopterId " + adopterId + " has succefully completed adoption history check");
 
     	//run criminal record background check - use the Hub verified background-check API call
-		LOGGER.info("adopterId " + adopterId " has succefully completed crinimal record check");
+		LOGGER.info("adopterId " + adopterId + " has succefully completed crinimal record check");
 
 		String displayResults = "<B>Congratulations !!!</B><BR><BR>You are cleared to adopt your next best friend<BR><BR><I>token:"+adopterId+"</I>";
 		
