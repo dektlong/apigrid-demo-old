f2027f6a59d881051117c4abf6b38588386fe849
dekt <dekel@vmware.com>
2021-03-06 09:45:37 -0800

    change tss to acc

---
+++ acc/run-local-acc-server.sh
@@ -0,0 +1,4 @@
+java -jar ~/Dropbox/Work/code/tss/tss-server/target/tss-server-1.0.0-SNAPSHOT.jar --tss.server.import-on-startup=true
+
+#architects: localhost:8877/admin/#/starters
+#devs: localhost:8877/dashboard/#/
