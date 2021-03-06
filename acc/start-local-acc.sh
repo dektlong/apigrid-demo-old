3b70ba2b6423a819045b0000a523426f58046e39
dekt <dekel@vmware.com>
2021-03-06 15:58:55 -0800

    add import examples to local acc

---
+++ acc/start-local-acc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+open -a Terminal ~/Dropbox/Work/code/tss/run-server.sh
+
+sleep 10
+
+acc/add-examples.sh generic
+
+open http://apps.dekt-dev.io:8877/dashboard/
+
+#architects: localhost:8877/admin/#/starters
+#devs: localhost:8877/dashboard/#/
