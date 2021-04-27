fbd81369d68306add132458080a9fc972f3984ee
dekt <dekel@vmware.com>
2021-04-26 18:15:48 -0700

    structure change - supplychain and workloads

--- tanzu-apps.sh
+++ tanzu-apps.sh
@@ -77,9 +77,9 @@ patch-backend() {
     echo
     
     #workaround for image refresh issue
-    kubectl delete -f workload/backend/dekt4pets-backend-app.yaml -n $APP_NAMESPACE > /dev/null 
+    kubectl delete -f workloads/backend/dekt4pets-backend-app.yaml -n $APP_NAMESPACE > /dev/null 
         
-    kustomize build workload/backend | kubectl apply -f -
+    kustomize build workloads/backend | kubectl apply -f -
 
 }
 
