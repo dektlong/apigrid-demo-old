e102f5681aeaa4a74a2fe29017c1106e1ad4ff5e
dekt <dekel@vmware.com>
2021-05-14 10:59:04 -0700

    remove generators

--- supply-chain/acc/add-examples.sh
+++ supply-chain/acc/add-examples.sh
@@ -10,33 +10,11 @@ init () {
     acc login -u admin -p admin
 }
 
-#add spring examples
-add-spring-examples() {
-
-    GENERATORS_FILES=("boot-api-k8s" "boot-k8s-kubectl" "boot-k8s-jpa" "boot-k8s-kustomize" "knative-kpack" "boot-knative-graalvm" "boot-knative-jib" "boot-cloudfoundry" "docker-compose" "web-k8s" "steeltoe-k8s-skaffold" "steeltoe-azure-spring-cloud")
-    ACCELERATORS_FILES=("frontend-ux-for-online-stores" "backend-api-for-online-stores" "supplychain-dataservices" "supplychain-microservices" "supplychain-api-gateways" "music-store-steeltoe" "ebanking" "todo-service" "function-kafka" "spring-jpa" "ruby-simple" "dotnet-aspnet-hello" "boot-function-knative")
-
-    import-files
-}
-
-#add dotnet examples
-add-dotnet-examples() {
-
-    GENERATORS_FILES=("boot-api-k8s" "web-k8s" "aks-resource-simple" "boot-aks-data" "boot-asc" "boot-function-azure" "boot-cloudfoundry" "docker-compose" "dotnet-web-azure" "kubernetes-steeltoe-skaffold" "steeltoe-asc" "boot-knative-graalvm")
-    ACCELERATORS_FILES=("frontend-ux-for-online-stores" "backend-api-for-online-stores" "supplychain-for-online-stores" "micro-gateways" "dotnet-SQLServer-EFCore" "music-store-steeltoe" "todo-service-asc" "jpa-sqlserver" "spring-cosmosdb-jpa" "dotnet-aspnet-hello" "boot-function-azure")
-
-    import-files
-}
-
-#import-files
-import-files() {
-
-    for i in ${!GENERATORS_FILES[@]}; do
-        acc generator create-from-file --file=supply-chain/acc/examples/generators/${GENERATORS_FILES[$i]}.yaml
-    done
+#import-accelerators
+import-accelerators() {
 
     for i in ${!ACCELERATORS_FILES[@]}; do
-        acc accelerator create-from-file --file=supply-chain/acc/examples/accelerators/${ACCELERATORS_FILES[$i]}.yaml
+        acc accelerator create-from-file --file=supply-chain/acc/accelerators/${ACCELERATORS_FILES[$i]}.yaml
     done
 }
 
@@ -44,11 +22,19 @@ import-files() {
 case $1 in
 spring)
     init $2
-    add-spring-examples
+    ACCELERATORS_FILES=("frontend-ux-for-online-stores" "backend-api-for-online-stores" \
+                        "supplychain-dataservices" "supplychain-microservices" "supplychain-api-gateways" \
+                        "music-store-steeltoe" "ebanking"  "todo-service" \
+                        "function-kafka" "spring-jpa" "ruby-simple" "dotnet-aspnet-hello" "boot-function-knative")
+    import-accelerators
     ;;
 dotnet)
     init $2
-    add-dotnet-examples
+    ACCELERATORS_FILES=("frontend-ux-for-online-stores" "backend-api-for-online-stores" \
+                        "supplychain-dataservices" "supplychain-microservices" "supplychain-api-gateways" \
+                        "music-store-steeltoe" "ebanking" "todo-service-asc" \ 
+                        "dotnet-SQLServer-EFCore" "jpa-sqlserver" "spring-cosmosdb-jpa" "dotnet-aspnet-hello" "boot-function-azure")
+    import-accelerators
     ;;
 *)
     echo "Incorrect usage. Please specify spring | dotnet"
