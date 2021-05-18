808484a7c91ef612af9580224917e0fdcc696bc1
dekt <dekel@vmware.com>
2021-05-18 12:46:51 -0700

    add cors permission to gateway for portal to execute routes

--- demo-builder.sh
+++ demo-builder.sh
@@ -292,50 +292,38 @@
         echo "=========> Start dekt4pets micro-gateway with public access..."
         echo
         kustomize build supply-chain/gateway | kubectl apply -f -
-        kubectl apply -f supply-chain/gateway/config/dekt4pets-ingress.yaml -n $APP_NAMESPACE
 
-        setup-dekt4pets-examples
-
-    }
-
-    #setup-dekt4pets-examples
-    setup-dekt4pets-examples() {
-
-        
         echo
-        echo "===> Create dekt4pets builder and TBS images..."
+        echo "===> Create dekt4pets builder..."
         echo
-
         kp builder create $BUILDER_NAME -n $APP_NAMESPACE \
         --tag $IMG_REGISTRY_URL/$IMG_REGISTRY_APP_REPO/$BUILDER_NAME \
         --order supply-chain/tbs/dekt-builder-order.yaml \
         --stack full \
         --store default
     
-        
-        #backend images
+        echo
+        echo "===> Create dekt4pets-backend TBS image..."
+        echo        
         kp image create $BACKEND_TBS_IMAGE -n $APP_NAMESPACE \
         --tag $DET4PETS_BACKEND_IMAGE_LOCATION \
         --git $DEMO_APP_GIT_REPO  \
         --sub-path ./workload-backend \
         --git-revision main \
         --wait
-
         #--builder $BUILDER_NAME \
-
         
-        #frontend images
-
+        echo
+        echo "===> Create dekt4pets-frontend TBS image..."
+        echo
         #"!! since animals-frontend does *not* currently compile with TBS, 
         # as a workaround we relocate the image from springcloudservices/animal-rescue-frontend as part of core-images "
-    
         #kp image create $FRONTEND_TBS_IMAGE -n $APP_NAMESPACE \
         #--tag $DET4PETS_FRONTEND_IMAGE_LOCATION \
         #--git https://github.com/spring-cloud-services-samples/animal-rescue\
         #--git-revision main \
         #--sub-path ./frontend \
         #--wait
-    
     }
 
 #################### helpers functions #############
