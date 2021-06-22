4d9b081a18c761fe56a68bb73535c05685305074
dekt <dekel@vmware.com>
2021-06-22 11:02:12 -0700

    add accelerators generation outputs

--- accelerators/dekt4pets-workflow/builds/create-builder.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kp builder create dekt4pets-builder -n dekt4pets-ns \
-   --tag goharbor.io/dekt4pets/dekt4pets-builder \
-   --order build-order.yaml \
-   --stack base \
-   --store default
