27cfaba4b4df8ea48cc5f680414ec5eded9e23e0
dekt <dekel@vmware.com>
2021-08-15 19:35:00 -0700

    update adopter-check-function

---
+++ workloads/dekt4pets/adopter-check/src/main/java/com/example/hello/AdopterCheckFunction.java
@@ -0,0 +1,26 @@
+package com.example.hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class AdopterCheckFunction {
+
+    @Value("${TARGET:from-function}")
+    String target;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdopterCheckFunction.class, args);
+    }
+
+    @Bean
+    public Function<String, String> hello() {
+        return (in) -> {
+            return "Running adoption backgroud check using this API:" + in + ", Results: APPROVED" + ", Source: " + target;
+        };
+    }
+}
