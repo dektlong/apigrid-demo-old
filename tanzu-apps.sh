051678a9a6c15b6db4a4a676ba4fde1d4621f529
dekt <dekel@vmware.com>
2021-08-24 12:14:27 -0700

    minor

--- tanzu-apps.sh
+++ tanzu-apps.sh
@@ -270,22 +270,19 @@ supplychain-dekt4pets() {
     echo "${bold}dekt4pets supplychain${normal}"
     echo "---------------------"
     echo
-    echo
-    echo "${bold}Workload Repositories${normal}"
+    echo "${bold}1. Workload Repositories${normal}"
     echo
     echo "NAME                      URL                                               STATUS"
     echo "dekt4pets-backend         https://github.com/dektlong/dekt4pets-backend     Fetched revision: main"
     echo "dekt4pets-frontend        https://github.com/dektlong/dekt4pets-frontend    Fetched revision: main"
     echo
-    echo
-    echo "${bold}Workload Images${normal}"
+    echo "${bold}2. Workload Images${normal}"
     echo
     kp images list -n $APP_NAMESPACE
-    echo "${bold}Cluster Builders${normal}"
-    echo
+    echo "${bold}3. Cluster Builders${normal}"
     echo
     kp builder list -n $APP_NAMESPACE
-    echo "${bold}Delivery Flow${normal}"
+    echo "${bold}4. Delivery Flow${normal}"
     echo
     echo "NAME                          KIND                PATH"
     echo "dekt4pets-backend             app                 workloads/dekt4pets/backend/config/dekt4pets-backend-app.yaml"
